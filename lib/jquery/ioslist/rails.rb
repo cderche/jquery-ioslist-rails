a34ac2cce573fc2ec42da6a3f3c255fd4f28935c
Kirill <c.derche@me.com>
2017-05-07 15:21:12 +0300

    .2

--- lib/jquery/ioslist/rails.rb
+++ lib/jquery/ioslist/rails.rb
@@ -1,14 +1,8 @@
-require "jquery/ioslist/rails/version"
-
 module Jquery
   module Ioslist
     module Rails
-      class Engine < ::Rails::Engine
-        initializer 'Precompile hook', :group => :all do |app|
-          app.config.assets.precompile += ['jquery.isolist.js']
-          app.config.assets.precompile += ['jquery.isolist.css']
-        end
-      end
+      require 'jquery/ioslist/rails/engine'
+      require 'jquery/ioslist/rails/version'
     end
   end
 end
---
+++ lib/jquery/ioslist/rails/engine.rb
@@ -0,0 +1,12 @@
+module Jquery
+  module Ioslist
+    module Rails
+      class Engine < ::Rails::Engine
+        initializer 'Precompile hook', :group => :all do |app|
+          app.config.assets.precompile += ['jquery.isolist.js']
+          app.config.assets.precompile += ['jquery.isolist.css']
+        end
+      end
+    end
+  end
+end
